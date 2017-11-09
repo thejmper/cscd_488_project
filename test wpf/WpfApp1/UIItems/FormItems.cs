8c8ef8012f189bf7be2e7cb5be090fc70747d757
Nick Huff <nhuff808@gmail.com>
2017-11-09 15:36:17 -0800

    Tested adding to canvsa using coords
    Will use canvas to draw on a tab. in a constructor(int xCoord, int Y coord, height of object, width of object).
    
    All UI objects inherit from a super so its a blck box solution

---
+++ test wpf/WpfApp1/UIItems/FormItems.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UIItems
+{
+    public class FormItems
+    {
+        private List<UIItem> items;
+
+        public FormItems()
+        {
+            items = new List<UIItem>();
+        }
+
+        public List<UIItem> Items { get => items; }
+
+        public void AddItem(UIItem item)
+        {
+            if (item != null)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+
+    
+}
--- test wpf/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ test wpf/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UIItems;
+using UIItems.ItemClasses;
 
 namespace WpfApp1
 {
@@ -24,100 +26,90 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            
-            for (int i = 0; i < 5; i++)
+
+            /*   for (int i = 0; i < 5; i++)
+             {
+                 var column = new ColumnDefinition();
+                 column.MinWidth = 300.0;
+                 column.MaxWidth = 300.0;
+                 gridd.ColumnDefinitions.Add(column);
+             }
+             for (int i = 0; i < 5; i++)
+             {
+                 var row = new RowDefinition();
+                 row.MinHeight = 100.0;
+                 row.MaxHeight = 100.0;
+                 gridd.RowDefinitions.Add(row);
+             }
+             for (int i = 0; i < 5; i++)
+             {
+                 for (int j = 0; j < 5; j++)
+                 {
+                     Button button = new Button();
+                     button.Content = "Button" + i;
+                     Grid.SetColumn(button, i);
+                     Grid.SetRow(button, j);
+                     gridd.Children.Add(button);
+                 }
+
+
+                 //x = (x +1) % rows;
+                // y = (y + 1)% col;
+             }
+             FormGrid fg = new FormGrid(5, 5);
+
+             for (int i = 0; i < col; i++)
+             {
+                 var column = new ColumnDefinition();
+                 column.MinWidth = 300.0;
+                 column.MaxWidth = 300.0;
+                 this.ColumnDefinitions.Add(column);
+             }
+             for (int i = 0; i < rows; i++)
+             {
+                 var row = new RowDefinition();
+                 row.MinHeight = 300.0;
+                 row.MaxHeight = 300.0;
+                 this.RowDefinitions.Add(row);
+             }
+             //gr = fg;
+             //this.Tab.Content = fg;
+             //this.Tab.Content = new Button();
+             Button dynamicButton = new Button();
+              dynamicButton.Width = 240;
+              dynamicButton.Height = 40;
+              dynamicButton.Margin = new Thickness(100, 21, 0, 0);
+              dynamicButton.Content = "Click me";
+              Grid.SetRow(dynamicButton, 0);
+              Grid.SetColumn(dynamicButton, 3);
+
+              //this.grid.Children.Add(dynamicButton);
+
+              dynamicButton.Click += button1_Click;
+
+              Label dynamicLabel = new Label();
+
+              dynamicLabel.Name = "NewLabel";
+              dynamicLabel.Content = "TEST";
+              dynamicLabel.Width = 240;
+              dynamicLabel.Height = 30;
+              dynamicLabel.Margin = new Thickness(0, 21, 0, 0);
+              dynamicLabel.Foreground = new SolidColorBrush(Colors.White);
+              dynamicLabel.Background = new SolidColorBrush(Colors.Black);
+
+              Grid.SetRow(dynamicLabel, 0);
+              Grid.SetColumn(dynamicLabel, 0);
+
+              //this.grid.Children.Add(dynamicLabel);*/
+
+            FormItems fi = new FormItems();
+            fi.AddItem(new TextBoxUI(100,0, 100,200));
+            foreach (UIItem item in fi.Items)
             {
-                var column = new ColumnDefinition();
-                column.MinWidth = 300.0;
-                column.MaxWidth = 300.0;
-                gridd.ColumnDefinitions.Add(column);
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                var row = new RowDefinition();
-                row.MinHeight = 100.0;
-                row.MaxHeight = 100.0;
-                gridd.RowDefinitions.Add(row);
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Button button = new Button();
-                    button.Content = "Button" + i;
-                    Grid.SetColumn(button, i);
-                    Grid.SetRow(button, j);
-                    gridd.Children.Add(button);
-                }
+                canvas.Children.Add(item.Element);
                 
-                
-                //x = (x +1) % rows;
-               // y = (y + 1)% col;
             }
-            /*  FormGrid fg = new FormGrid(5, 5);
 
-            for (int i = 0; i < col; i++)
-            {
-                var column = new ColumnDefinition();
-                column.MinWidth = 300.0;
-                column.MaxWidth = 300.0;
-                this.ColumnDefinitions.Add(column);
-            }
-            for (int i = 0; i < rows; i++)
-            {
-                var row = new RowDefinition();
-                row.MinHeight = 300.0;
-                row.MaxHeight = 300.0;
-                this.RowDefinitions.Add(row);
-            }
-            //gr = fg;
-            //this.Tab.Content = fg;
-            //this.Tab.Content = new Button();
-            Button dynamicButton = new Button();
-             dynamicButton.Width = 240;
-             dynamicButton.Height = 40;
-             dynamicButton.Margin = new Thickness(100, 21, 0, 0);
-             dynamicButton.Content = "Click me";
-             Grid.SetRow(dynamicButton, 0);
-             Grid.SetColumn(dynamicButton, 3);
-
-             //this.grid.Children.Add(dynamicButton);
-
-             dynamicButton.Click += button1_Click;
-
-             Label dynamicLabel = new Label();
-
-             dynamicLabel.Name = "NewLabel";
-             dynamicLabel.Content = "TEST";
-             dynamicLabel.Width = 240;
-             dynamicLabel.Height = 30;
-             dynamicLabel.Margin = new Thickness(0, 21, 0, 0);
-             dynamicLabel.Foreground = new SolidColorBrush(Colors.White);
-             dynamicLabel.Background = new SolidColorBrush(Colors.Black);
-
-             Grid.SetRow(dynamicLabel, 0);
-             Grid.SetColumn(dynamicLabel, 0);
-
-             //this.grid.Children.Add(dynamicLabel);*/
-
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            Label dynamicLabel = new Label();
-
-            dynamicLabel.Name = "NewLabel";
-            dynamicLabel.Content = "TEST";
-            dynamicLabel.Width = 240;
-            dynamicLabel.Height = 30;
-            dynamicLabel.Margin = new Thickness(0, 21, 0, 0);
-            dynamicLabel.Foreground = new SolidColorBrush(Colors.White);
-            dynamicLabel.Background = new SolidColorBrush(Colors.Black);
-
-            Grid.SetRow(dynamicLabel, 1);
-            Grid.SetColumn(dynamicLabel, 0);
-
-            //this.grid.Children.Add(dynamicLabel);
         }
     }
 
@@ -141,7 +133,7 @@ namespace WpfApp1
                 row.MaxHeight = 300.0;
                 this.RowDefinitions.Add(row);
             }
-            int x = 0, y=0;
+          
 
             /*for (int i = 0; i < rows * col; i++)
             {
