ad8c513ebc17d6462d48dd54ddf3884a0a479c40
Nick Huff <nhuff808@gmail.com>
2017-12-02 13:53:37 -0800

    Created ControlDate and ControlBoolean
    ControlDate manages DatePicker tools and ControlBoolean uses CheckBoxs

---
+++ WpfApp1/WpfApp1/FormItems/ControlBoolean.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml;
+
+namespace WpfApp1.FormItems
+{
+    public class ControlBoolean : FormControl<CheckBox, bool>
+    {
+        //--member fields--//
+        private BoolDataHolder dataHolder;
+
+        //--construction--//
+        public ControlBoolean(string name, string engishTitle, Orientation orientation = Orientation.Vertical) : base(name, engishTitle, new CheckBox(), orientation)
+        {
+        }
+
+        protected ControlBoolean() : base("untitledControlDate", "untitled date control", new CheckBox())
+        {
+
+        }
+
+        protected override void BindControl()
+        {
+            this.dataHolder = new BoolDataHolder();
+
+            this.binding = new Binding("value");
+            binding.Source = dataHolder;
+            control.SetBinding(CheckBox.IsCheckedProperty, binding);
+        }
+
+        //--cloning--//
+        public override FormElement Clone()
+        {
+            ControlBoolean clone = new ControlBoolean(this.name, this.englishTitle, this.orientation);
+            clone.dataHolder.value = this.dataHolder.value;
+            (clone.control).GetBindingExpression(DatePicker.TextProperty).UpdateTarget();
+
+            return clone;
+        }
+
+        public override void SetControl(bool value)
+        {
+            dataHolder.value = value;
+            (control).GetBindingExpression(CheckBox.IsCheckedProperty).UpdateTarget();
+        }
+
+        protected override void SetReadOnlyInternal(bool isReadOnly)
+        {
+            this.control.IsEnabled = !isReadOnly;
+        }
+
+        protected override void WriteControl(XmlWriter writer)
+        {
+            (control).GetBindingExpression(CheckBox.IsCheckedProperty).UpdateSource();
+            if (this.dataHolder.value)
+            {
+                writer.WriteElementString("value", "true");
+            }
+            else
+            {
+                writer.WriteElementString("value", "false");
+            }
+        }
+
+        protected override void ReadControl(XmlReader reader)
+        {
+            String valueString = reader.ReadElementContentAsString();
+            if (valueString == "true")
+            {
+                this.SetControl(true);
+            }
+            else
+            {
+                this.SetControl(false);
+            }
+        }
+
+        /// <summary>
+        /// we need a class to put our data, not just a reference, because otherwise
+        /// the WPF doesn't update right and generally does silly things.
+        /// </summary>
+        private class BoolDataHolder
+        {
+            public bool value { get; set; }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/FormItems/ControlDate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml;
+
+namespace WpfApp1.FormItems
+{
+    public class ControlDate : FormControl<DatePicker, DateTime>
+    {
+        //--member fields--//
+        private DateDataHolder dataHolder;
+
+        //--construction--//
+        public ControlDate(string name, string engishTitle, Orientation orientation = Orientation.Vertical) : base(name, engishTitle, new DatePicker(), orientation)
+        {
+        }
+
+        protected ControlDate(): base("untitledControlDate", "untitled date control", new DatePicker())
+        {
+
+        }
+
+        protected override void BindControl()
+        {
+            this.dataHolder = new DateDataHolder();
+
+            this.binding = new Binding("value");
+            binding.Source = dataHolder;
+            control.SetBinding(DatePicker.TextProperty, binding);
+        }
+
+        //--cloning--//
+        public override FormElement Clone()
+        {
+            ControlDate clone = new ControlDate(this.name, this.englishTitle, this.orientation);
+            clone.dataHolder.date = this.dataHolder.date;
+            (clone.control).GetBindingExpression(DatePicker.TextProperty).UpdateTarget();
+
+            return clone;
+        }
+
+        public override void SetControl(DateTime value)
+        {
+            dataHolder.date = value;
+            (control).GetBindingExpression(DatePicker.TextProperty).UpdateTarget();
+        }
+
+        protected override void SetReadOnlyInternal(bool isReadOnly)
+        {
+            this.control.IsEnabled = !isReadOnly;
+        }
+
+        protected override void WriteControl(XmlWriter writer)
+        {
+            (control).GetBindingExpression(DatePicker.TextProperty).UpdateSource();
+            writer.WriteElementString("text", this.dataHolder.date.ToShortDateString());
+        }
+
+        protected override void ReadControl(XmlReader reader)
+        {
+            String dateString = reader.ReadElementContentAsString();
+            string [] parts = dateString.Split('/');
+            int year;
+            Int32.TryParse(parts[2], out year);
+            int month;
+            Int32.TryParse(parts[0], out month);
+            int day;
+            Int32.TryParse(parts[1], out day);
+
+            DateTime date = new DateTime(year, month, day);
+            this.SetControl(date);
+        }
+
+        /// <summary>
+        /// we need a class to put our data, not just a reference, because otherwise
+        /// the WPF doesn't update right and generally does silly things.
+        /// </summary>
+        private class DateDataHolder
+        {
+            public DateTime date { get; set; }
+        }
+    }
+}
